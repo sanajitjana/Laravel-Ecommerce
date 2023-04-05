45903630bd9ea417b06b4fffcde4325e908f5516
SanajitJana <sanajit.jana@mplifytech.com>
2023-04-05 18:03:08 +0530

    Create admin api route

--- routes/api/admin.php
+++ routes/api/admin.php
@@ -2,36 +2,22 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
- */
-
 Route::group(['namespace' => 'api\admin', 'prefix' => 'admin.'], function () {
 
     /*authentication*/
-    Route::group(['namespace' => 'Auth', 'prefix' => 'auth', 'as' => 'auth.'], function () {
-        Route::get('login', 'LoginController@login')->name('login');
-        Route::get('logout', 'LoginController@logout')->name('logout');
+    Route::group(['namespace' => 'Auth', 'prefix' => 'auth'], function () {
     });
 
     /*authenticated*/
     Route::group(['middleware' => ['admin']], function () {
 
         //dashboard routes
-        Route::group(['prefix' => 'dashboard', 'as' => 'dashboard.'], function () {
+        Route::group(['prefix' => 'dashboard'], function () {
             Route::get('/', 'DashboardController@dashboard')->name('index');
         });
 
         //reports routes
-        Route::group(['prefix' => 'reports', 'as' => 'reports.'], function () {
-            Route::get('/', 'ReportsController@index')->name('index');
+        Route::group(['prefix' => 'reports'], function () {
         });
     });
 });
